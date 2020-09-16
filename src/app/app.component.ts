23dffab7c2597e75f4740ca2b837f42451ebeb98
Seremet Radu <radu.seremet@gmail.com>
2020-09-16 18:17:08 +0300

    angular components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'cards';
+
+  posts = [
+    {
+      title: 'Neat Tree',
+      username: '@nature',
+      imageUrl: 'assets/tree.jpeg',
+      content: "This is some dommy text for testing purposes"
+    },
+    {
+      title: 'Biking',
+      username: '@biking',
+      imageUrl: 'assets/biking.jpeg',
+      content: "This is some dommy text for testing purposes about biking"
+    },
+    {
+      title: 'Snow Mountain',
+      username: '@mountain',
+      imageUrl: 'assets/mountain.jpeg',
+      content: "This is some dommy text for testing purposes about mountain"
+
+    },
+    {
+      title: 'Biking',
+      username: '@biking',
+      imageUrl: 'assets/biking.jpeg',
+      content: "This is some dommy text for testing purposes about biking"
+    }
+  ]
 }
